c7c25a303f71cf3548c243d43cb6bd26eebf0e98
Liam Quick <liam.quick@vcontractor.co.za>
2024-05-27 15:48:03 +0200

    Adding a basic nav bar with ThemeToggle.jsx

--- planning-web/src/App.jsx
+++ planning-web/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import NavBar from './NavBar/NavBar.jsx';
 
 /**
  * @returns {JSX.Element} Main App component
  */
 export default function App() {
   return (
-    <>
-    </>
+    <NavBar />
   );
 }
