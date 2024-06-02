c5bf80a69ef0a37bffb265a526c8fb3e93436242
Faheemah <faheemah@bbd.co.za>
2024-06-02 19:32:51 +0200

    Update apiRoutes.js

--- server/routes/apiRoutes.js
+++ server/routes/apiRoutes.js
@@ -8,11 +8,11 @@ const voteTypeRoutes = require('./voteTypeRoutes');
 const router = express.Router();
 const testRoutes = require('./testRoutes');
 
-router.use('/votes', voteRoutes);
-router.use('/vote-types', voteTypeRoutes);
 router.use('/', testRoutes);
 router.use('/users', userRoutes); 
 router.use('/rooms', roomRoutes); 
 router.use('/tickets', ticketRoutes); 
+router.use('/votes', voteRoutes);
+router.use('/vote-types', voteTypeRoutes);
 
 module.exports = router;
