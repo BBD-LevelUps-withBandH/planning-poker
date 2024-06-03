80aeeced82841ce10a720592de19e836b46561ae
Stefan de Jager <stefan.dejager@bbd.co.za>
2024-06-03 18:14:09 +0200

    fixing room uuid

--- database/addingUUID_fixed.sql
+++ database/addingUUID_fixed.sql
@@ -3,5 +3,8 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 --rollback DROP EXTENSION IF EXISTS "uuid-ossp";
 
 --changeset stefan:ddl:addColumnRoomUuid
+--preconditions onFail:MARK_RAN
+--precondition-sql-check expectedResult:false
+SELECT column_name FROM information_schema.columns WHERE table_name='rooms' AND column_name='room_uuid';
 ALTER TABLE rooms ADD COLUMN room_uuid UUID DEFAULT uuid_generate_v4();
 --rollback ALTER TABLE rooms DROP COLUMN room_uuid;
