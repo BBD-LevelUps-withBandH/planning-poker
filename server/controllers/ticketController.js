ea82b43845f40ddc8fc2ae4792efee9f6164c7df
Stefan de Jager <stefan.dejager@bbd.co.za>
2024-06-03 20:53:30 +0200

    remove unused endpoints

--- server/controllers/ticketController.js
+++ server/controllers/ticketController.js
@@ -1,16 +1,7 @@
-const { getAllTickets, updateTicket, createTicket, getTicketById } = require('../data/ticketRepository');
-const { getVotesByTicketId, createVote } = require('../data/voteRepository');
+const { createTicket } = require('../data/ticketRepository');
 const { handleErrors } = require('../middlewares/errorHandler');
 
 function ticketController(router) {
-  router.post(
-    '/',
-    handleErrors(async (req, res) => {
-      const ticket = req.body;
-      await updateTicket(ticket);
-      res.sendStatus(204);
-    })
-  );
 
   router.post(
     '/create',
@@ -18,15 +9,6 @@ function ticketController(router) {
       const { ticketName, roomId } = req.body;
       const newTicket = await createTicket(ticketName, roomId);
       res.status(201).json(newTicket);
-    })
-  );
-
-  router.get(
-    '/:id',
-    handleErrors(async (req, res) => {
-      const ticketId = req.params.id;
-      const ticket = await getTicketById(ticketId);
-      res.json(ticket);
     })
   );
 }
--- server/data/roomRepository.js
+++ server/data/roomRepository.js
@@ -3,26 +3,13 @@ const Room = require('../models/room');
 
 const tableName = 'rooms';
 
-const getAllRooms = async () => {
-  const result = await client.query(`SELECT * FROM ${tableName}`);
-  return result.rows.map(row => new Room(row.room_id, row.room_name, row.owner_id, row.closed));
-};
-
-const getRoomById = async (roomId) => {
-  const result = await client.query(`SELECT * FROM ${tableName} WHERE room_id = $1`, [roomId]);
+const getRoomById = async (roomUUID) => {
+  const result = await client.query(`SELECT owner_id FROM ${tableName} WHERE room_uuid = $1`, [roomUUID]);
   if (result.rows.length === 0) {
-    throw new Error(`Room with ID ${roomId} not found`);
+    throw new Error(`Room with ID ${roomUUID} not found`);
   }
   const row = result.rows[0];
-  return new Room(row.room_id, row.room_name, row.owner_id, row.closed);
-};
-
-const updateRoom = async (room) => {
-  const { roomId, roomName, ownerId, closed } = room;
-  await client.query(
-    `UPDATE ${tableName} SET room_name = $1, owner_id = $2, closed = $3 WHERE room_id = $4`,
-    [roomName, ownerId, closed, roomId]
-  );
+  return {ownerId: row.owner_id};
 };
 
 const createRoom = async (roomName, ownerId, closed = false) => {
@@ -35,8 +22,6 @@ const createRoom = async (roomName, ownerId, closed = false) => {
 };
 
 module.exports = {
-  getAllRooms,
   getRoomById,
-  updateRoom,
   createRoom,
 };
