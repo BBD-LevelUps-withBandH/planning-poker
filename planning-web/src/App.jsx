7fcc2b572747874f6ea90a3f03981f6cd3c4dbf5
Moshe-Brill <mosheahron.brill@vcontractor.co.za>
2024-06-03 19:09:06 +0200

    localhost working before pull

--- planning-web/src/App.jsx
+++ planning-web/src/App.jsx
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './NavBar/NavBar.jsx';
+import AuthHandler from './AuthHandler.jsx';
 
-/**
- * @returns {JSX.Element} Main App component
- */
+function HomePage() {
+  useEffect(() => {
+    const accessToken = localStorage.getItem('access_token');
+    const idToken = localStorage.getItem('id_token');
+
+    console.log('Access Token on Home Page:', accessToken); // Log to see the access token on home page
+    console.log('ID Token on Home Page:', idToken); // Log to see the ID token on home page
+  }, []);
+
+  const manualSignIn = () => {
+    const cognitoDomain = import.meta.env.VITE_COGNITO_DOMAIN;
+    const redirectUri = import.meta.env.VITE_REDIRECT_URI;
+    const clientId = import.meta.env.VITE_CLIENT_ID;
+    const url = `${cognitoDomain}/oauth2/authorize?identity_provider=Google&redirect_uri=${redirectUri}&response_type=token&client_id=${clientId}&scope=email openid phone`;
+    window.location.href = url;
+  };
+
+  return (
+    <div>
+      <button onClick={manualSignIn}>Login</button>
+    </div>
+  );
+}
+
 export default function App() {
   return (
-    <NavBar />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path="/auth" element={<AuthHandler />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </Router>
   );
 }
