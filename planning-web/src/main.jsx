4c7d54b1b79d5c40fc7822af57b4bc81de97dd50
Liam Quick <liam.quick@vcontractor.co.za>
2024-06-04 08:27:52 +0200

    re-adding lint disable

--- planning-web/src/main.jsx
+++ planning-web/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client.js';
+/* eslint-disable node/file-extension-in-import */
+import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 
