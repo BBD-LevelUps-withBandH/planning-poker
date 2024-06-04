14398461b47577185cdf38dfc1170a88e9b35a60
Stefan de Jager <stefan.dejager@bbd.co.za>
2024-06-05 00:54:57 +0200

    fixing my silly oopsie

--- server/data/roomRepository.js
+++ server/data/roomRepository.js
@@ -32,7 +32,7 @@ const createRoom = async (roomName, upn, closed = false) => {
 const updateRoomTicket = async (roomUuid, ticketId) => {
   const query = `UPDATE ${tableName} SET current_ticket_id = $1 WHERE room_id = (SELECT room_id FROM ${tableName} WHERE room_uuid = $2)`;
   await client.query(query, [ticketId, roomUuid]);
-  return { roomUuid: row.room_uuid };
+  return { roomUuid: roomUuid };
 }
 
 module.exports = {
