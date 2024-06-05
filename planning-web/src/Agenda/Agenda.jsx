bd7d224a857c1eeee637eab960d1444b42f9a3ce
Liam Quick <liam.quick@vcontractor.co.za>
2024-06-05 08:42:57 +0200

    Fixing agenda not showing revealed score for current ticket Adding case for no numbered votes

--- planning-web/src/Agenda/Agenda.jsx
+++ planning-web/src/Agenda/Agenda.jsx
@@ -23,19 +23,29 @@ export default function Agenda({ tickets, setTickets, currentTopic, user, votes,
    * @param {number} index - index of element in array
    * @returns {JSX.Element} Ticket Body
    */
-  function getTicketBody({ ticketName, ticketId }, index) {
+  function getTicketBody({ ticketName, ticketId, revealed }, index) {
     const ticketVotes = votes.filter(vote => vote.ticketId === ticketId && !Number.isNaN(Number(vote.vote)));
+    const noNumberVotes = revealed && ticketVotes.length === 0;
     return (
       <section
         className={ `container-v${currentTopic?.ticketId === ticketId ? ' current' : ''}` }
         key={ index }
       >
         <li>{ticketName}</li>
-        <p>{
+        <p>
+        {
           ticketVotes.length > 0
-          && tickets.indexOf(currentTopic) > index
+          && revealed
           && (ticketVotes.reduce((sum, { vote }) => sum + Number(vote), 0) / ticketVotes.length).toFixed(1)
-        }</p>
+        }
+          {
+            noNumberVotes && ticketVotes.filter(vote => vote.ticketId === ticketId).reduce((acc, {vote}) => {
+              acc[vote] = (acc[vote] || 0) + 1;
+              if (!acc.max || acc[vote] > acc[acc.max]) acc.max = vote;
+              return acc;
+            }, { max: '?'}).max
+          }
+        </p>
       </section>
     );
   }
